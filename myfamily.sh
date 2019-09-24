897c49fd9ad2e0c9a7b8d785b5565082832ed569
alitlekbay <alitlekbay@yandex.ru>
2019-09-24 08:33:54 -0400

    g

--- myfamily.sh
+++ myfamily.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq -r ".[] | select(.id==$HERO_ID) | .connections | .relatives"
+curl https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq -jr ".[] | select(.id==$HERO_ID) | .connections | .relatives"
