aa9ca06d9c90bf3fbbc9362aeebd12f9f5aaeadb
yenlik29 <bokina29@gmail.com>
2019-09-26 14:12:27 +0600

    gfddf

---
+++ printnbr.go
@@ -0,0 +1,19 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintNbr(n int32) {
+	if n == 0 {
+		z01.PrintRune('0')
+		return
+	}
+	if n < 0 {
+		z01.PrintRune('-')
+		n = n*-1
+	}
+	if n >= 10 {
+		PrintNbr(n/10)
+	}
+	z01.PrintRune(n % 10 + '0')
+	return
+}
