8c5f5d264c34dd40897ac644ca9bbf5b9633ce21
yenlik29 <bokina29@gmail.com>
2019-09-26 18:48:40 +0600

    gdf

---
+++ basicatoi.go
@@ -0,0 +1,39 @@
+package piscine
+
+func toInt(n byte) int {
+	if n == 1 {
+		return (1)
+	} else if n == 2 {
+		return (2)
+	} else if n == 3 {
+		return (3)
+	} else if n == 4 {
+		return (4)
+	} else if n == 5 {
+		return (5)
+	} else if n == 6 {
+		return (6)
+	} else if n == 7 {
+		return (7)
+	} else if n == 8 {
+		return (8)
+	} else if n == 9 {
+		return (9)
+	}
+	return 0
+}
+
+func BasicAtoi(s string) int {
+	
+	newstr := []byte(s)
+	count := 0
+	for _, _ = range newstr {
+		count++
+	}
+
+	result := 0
+	for i := 0; i < count; i++ {
+		result = result * 10 + toInt((newstr[i]) - '0')
+	}
+	return result
+}
--- test/main.go
+++ test/main.go
@@ -6,7 +6,15 @@ import (
 )
 
 func main() {
-	s := "Hello World!"
-	s = piscine.StrRev(s)
-	fmt.Println(s)
+	s := "12345"
+	s2 := "0000000012345"
+	s3 := "000000"
+
+	n := piscine.BasicAtoi(s)
+	n2 := piscine.BasicAtoi(s2)
+	n3 := piscine.BasicAtoi(s3)
+
+	fmt.Println(n)
+	fmt.Println(n2)
+	fmt.Println(n3)
 }
