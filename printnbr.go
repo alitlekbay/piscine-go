fccb86968c5f409c1c1429a712a208b18572932a
yenlik29 <bokina29@gmail.com>
2019-09-26 14:24:27 +0600

    gfd

--- printnbr.go
+++ printnbr.go
@@ -2,6 +2,29 @@ package piscine
 
 import "github.com/01-edu/z01"
 
+func PrintIt(n int) rune {
+	if n == 1 {
+		return ('1')
+	} else if n == 2 {
+		return('2')
+	} else if n == 3 {
+		return('3')
+	} else if n == 4 {
+		return('4')
+	} else if n == 5 {
+		return('5')
+	} else if n == 6 {
+		return('6')
+	} else if n == 7 {
+		return('7')
+	} else if n == 8 {
+		return('8')
+	} else if n == 9 {
+		return('9')
+	}
+	return '0'
+}
+
 func PrintNbr(n int) {
 	if n == 0 {
 		z01.PrintRune('0')
@@ -14,6 +37,6 @@ func PrintNbr(n int) {
 	if n >= 10 {
 		PrintNbr(n/10)
 	}
-	z01.PrintRune(rune(n % 10 + '0'))
+	z01.PrintRune(PrintIt(n % 10))
 	return
 }
--- test/main.go
+++ test/main.go
@@ -3,7 +3,7 @@ package main
 import piscine ".."
 
 func main() {
-	piscine.PrintNbr(1235644)
+	piscine.PrintNbr(123)
 	//z01.PrintRune('\n')
 	// piscine.PrintComb2()
 }
