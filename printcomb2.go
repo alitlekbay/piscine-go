2ac811376308afa16895cba53a0175072f375d0d
yenlik29 <bokina29@gmail.com>
2019-09-26 14:09:34 +0600

    hfg

--- printcomb2.go
+++ printcomb2.go
@@ -2,55 +2,45 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-func formatint(i int) rune {
-	if i == 1 {
-		return ('1')
-	} else if i == 2 {
-		return ('2')
-	} else if i == 3 {
-		return ('3')
-	} else if i == 4 {
-		return ('4')
-	} else if i == 5 {
-		return ('5')
-	} else if i == 6 {
-		return ('6')
-	} else if i == 7 {
-		return ('7')
-	} else if i == 8 {
-		return ('8')
-	} else if i == 9 {
-		return ('9')
-	} else if i == 0 {
-		return ('0')
+func PrintNbr(n int32) {
+	if n == 0 {
+		z01.PrintRune('0')
+		return
 	}
-	return ('0')
+	if n < 0 {
+		z01.PrintRune('-')
+		n = n*-1
+	}
+	if n >= 10 {
+		PrintNbr(n/10)
+	}
+	z01.PrintRune(n % 10 + '0')
+	return
 }
 
 func PrintComb2() {
-	for i := 0; i <= 9; i++ {
-		for j := 0; j <= 9; j++ {
-			for l := 0; l <= 9; l++ {
-				for m := 0; m <= 9; m++ {
-					if i == 9 && j == 8 && l == 9 && m == 9 {
-						z01.PrintRune(formatint(i))
-						z01.PrintRune(formatint(j))
-						z01.PrintRune(32)
-						z01.PrintRune(formatint(l))
-						z01.PrintRune(formatint(m))
-						z01.PrintRune(10)
-						return
-					} else {
-						z01.PrintRune(formatint(i))
-						z01.PrintRune(formatint(j))
-						z01.PrintRune(32)
-						z01.PrintRune(formatint(l))
-						z01.PrintRune(formatint(m))
-						z01.PrintRune(44)
-						z01.PrintRune(32)
-					}
-				}
-			}
-		}
+	var i int32
+	var j int32
+	 for i = 0; i <= 99; i++ {
+	 	for j = 0; j <= 99; j++ {
+	 		if i < j {
+	 			if i < 10 {
+	 				z01.PrintRune('0')
+	 			}
+	 			PrintNbr(i)
+	 			z01.PrintRune(' ')
+	 			if j < 10 {
+	 				z01.PrintRune('0')
+	 			}
+	 			PrintNbr(j)
+	 			if i == 98 && j == 99 {
+	 				z01.PrintRune('\n')
+	 			} else {
+	 				z01.PrintRune(',')
+	 				z01.PrintRune(' ')
+	 			}
+	 		}
+	 	}
 	}
+
 }
--- test/main.go
+++ test/main.go
@@ -3,6 +3,8 @@ package main
 import piscine ".."
 
 func main() {
+	//piscine.PrintNbr(123-5644)
+	//z01.PrintRune('\n')
 	piscine.PrintComb2()
 }
 
