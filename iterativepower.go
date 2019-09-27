aed9ee86e5ed9bcde4cc7de3ba0f6c0f77a7e2cf
yenlik29 <bokina29@gmail.com>
2019-09-27 15:59:58 +0600

    gfd

--- iterativepower.go
+++ iterativepower.go
@@ -4,14 +4,12 @@ func IterativePower(nb int, power int) int {
 	if power < 0 {
 		return 0
 	}
-	if nb < 0 {
-		return 1
-	}
+
 	result := 1
 	for i := 0; i < power; i++ {
 		result = result * nb
 		if result < 0 {
-			return 0
+			return 1
 		} 
 	}
 	return result
