1a8ba28017fe68b215054e3e7ef2a640acc54ccb
yenlik29 <bokina29@gmail.com>
2019-09-27 16:23:03 +0600

    dgf

--- iterativepower.go
+++ iterativepower.go
@@ -2,6 +2,12 @@ package piscine
 
 func IterativePower(nb int, power int) int {
 	result := 1
+	if power < 0 {
+		return 0
+	}
+	if power == 0 {
+		return 1
+	}
 	for i := 1; i <= power; i++ {
 		result = result * nb
 	}
