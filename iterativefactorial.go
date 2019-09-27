63b29ecf56fd24895cb18922750a726998eed26a
kilney <yb@alem.ai>
2019-09-27 13:42:44 +0600

    gdf

---
+++ iterativefactorial.go
@@ -0,0 +1,12 @@
+package piscine
+
+func IterativeFactorial(nb int) int {
+     if nb < 0 {
+     	return 0
+     }
+     fact := 1
+     for i := 1; i <= nb; i++ {
+     	 fact *= i
+     }
+     return fact
+}
--- test/main.go
+++ test/main.go
@@ -6,15 +6,6 @@ import (
 )
 
 func main() {
-	s := "Invalid123"
-	s2 := "1Invalid123"
-	s3 := "123."
-
-	n := piscine.BasicAtoi2(s)
-	n2 := piscine.BasicAtoi2(s2)
-	n3 := piscine.BasicAtoi2(s3)
-
-	fmt.Println(n)
-	fmt.Println(n2)
-	fmt.Println(n3)
+     arg := 4
+     fmt.Println(piscine.IterativeFactorial(arg))
 }
