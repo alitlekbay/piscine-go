5fbe3522584f8fa28a4a08e4313c2090b5e68d31
yenlik29 <bokina29@gmail.com>
2019-09-27 15:57:38 +0600

    gdf

--- test/main.go
+++ test/main.go
@@ -9,4 +9,8 @@ func main() {
     arg1 := 4
 	arg2 := 3
 	fmt.Println(piscine.IterativePower(arg1, arg2))
+
+	fmt.Println(piscine.IterativePower(-1, 2))
+	fmt.Println(piscine.IterativePower(-1, 8))
+	fmt.Println(piscine.IterativePower(-4, 4))
 }
