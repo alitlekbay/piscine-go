6f4d4a746a6391b8c36fae8711da387978589b7b
yenlik29 <bokina29@gmail.com>
2019-09-27 16:42:24 +0600

    gf

--- sqrt.go
+++ sqrt.go
@@ -1,6 +1,9 @@
 package piscine
 
 func Sqrt(nb int) int {
+	if nb == 1 {
+		return 1
+	}
 	for i := 1; i < nb; i++ {
 		if i * i == nb {
 			return i
--- test/main.go
+++ test/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	arg1 := 4
-	arg2 := 7
+	arg2 := 1
 	fmt.Println(piscine.Sqrt(arg1))
 	fmt.Println(piscine.Sqrt(arg2))
 }
