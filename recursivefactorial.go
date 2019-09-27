697f358307d3c68855e8e4d4f8d70a49a3b3c016
yenlik29 <bokina29@gmail.com>
2019-09-27 15:48:03 +0600

    gdgdf

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -4,6 +4,9 @@ func RecursiveFactorial(nb int) int {
 	if nb < 0 {
 		return 0
 	}
+	if nb >= 21 {
+		return 0
+	}
 	if nb >= 1 {
 		result := nb * RecursiveFactorial(nb - 1)
 		if result < 0 {
--- test/main.go
+++ test/main.go
@@ -6,8 +6,8 @@ import (
 )
 
 func main() {
-    for i:=1;i<1000;i++ {
-		// piscine.RecursiveFactorial(i)
+    for i:=1;i<999999999;i++ {
+		piscine.RecursiveFactorial(i)
     	fmt.Println(i, piscine.RecursiveFactorial(i))
 	}
 }
