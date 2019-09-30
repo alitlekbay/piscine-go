7425c5f4b3f3ea170e3f82b8905c0d9e57a39051
yenlik29 <bokina29@gmail.com>
2019-09-30 16:24:08 +0600

    fd

--- index.go
+++ index.go
@@ -1,11 +1,24 @@
 package piscine
 
+// import "fmt"
+
+func StrLen(str string) int {
+	count := 0
+	for _, _ = range str {
+		count++
+	}
+	return count
+}
+
 func Index(s string, toFind string) int {
+	match := -1
 	lol := []rune(toFind)
 	for k, val := range s {
 		if val == lol[0] {
-			return k
+			match = k
+		} else {
+			continue
 		}
 	}
-	return -1
+	return match
 }
--- test/main.go
+++ test/main.go
@@ -9,4 +9,5 @@ func main() {
 	fmt.Println(piscine.Index("Hello!", "l"))
 	fmt.Println(piscine.Index("Salut!", "alu"))
 	fmt.Println(piscine.Index("Ola!", "hOl"))
+	fmt.Println(piscine.Index("k]7Nz2Nxr70/Z", "Nxr7"))
 }
